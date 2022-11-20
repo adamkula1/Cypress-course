99f76befc88a99e2c7107421cf12009438eeac7a
Adam Kula <adamkula97@gmail.com>
2022-11-20 13:47:40 +0100

    task #07 - contains, timeout

--- cypress/integration/07_retry_logika_vztahy_medzi_prikazmi/zaciatok.js
+++ cypress/integration/07_retry_logika_vztahy_medzi_prikazmi/zaciatok.js
@@ -1,20 +1,22 @@
 /// <reference types="cypress" />
 
-beforeEach( () => {
-
-  cy
-    .visit('/board/25619573353');
-
+beforeEach(() => {
+  cy.visit("/board/82501116053");
 });
 
-it('v zozname je mlieko', () => {
+it("v zozname je mlieko", () => {
+  cy.contains("mlieko");
 
+  cy.getByData('24588109879').contains("mlieko");
 });
 
-it('v druhom zozname je mlieko', () => {
-
+it("v druhom zozname je mlieko", () => {
+  cy.getByData('12565371934')
+    .find(".Task")
+    .eq(2)
+    .should("contain.text", "mlieko");
 });
 
-it('v druhom zozname je len jeden task', () => {
-
+it("v druhom zozname je len jeden task", () => {
+  cy.get('[data-id="12565371934"] .Task').should("have.length", 3);
 });
--- cypress/integration/07_retry_logika_vztahy_medzi_prikazmi/zoznamsa.js
+++ cypress/integration/07_retry_logika_vztahy_medzi_prikazmi/zoznamsa.js
@@ -1,18 +1,19 @@
 /// <reference types="cypress" />
 
-beforeEach( () => {
-
-  cy
-    .visit(); // doplň si adresu svojho boardu
-
+beforeEach(() => {
+  cy.visit("/board/82501116053"); // doplň si adresu svojho boardu
 });
 
 // ⚠️ pred začiatkom testovania si prosím vytvor dva zoznamy
 // použi príkaz .contains()
-it('pridanie položky do druhého zoznamu', () => {
+it("pridanie položky do druhého zoznamu", () => {
+  cy.get(".List").eq(1).contains("Add a card").click();
+  cy.getByData("newTaskTitle").eq(1).click().type("Rožok{enter}");
+});
 
-})
-
-it('nastavenie timeoutu', () => {
-
-})
+it("nastavenie timeoutu", () => {
+  cy.get('[data-id="12565371934"] .Task', { timeout: 3000 }).should(
+    "have.length",
+    4
+  );
+});
