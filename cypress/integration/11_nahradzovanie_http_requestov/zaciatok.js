a2c4f85bbae323795dfc53d2442724bea3dc1145
Adam Kula <adamkula97@gmail.com>
2022-11-20 16:16:34 +0100

    task #11 - Stubbed - nahradzovanie http requestov, check error message

--- cypress/integration/11_nahradzovanie_http_requestov/zaciatok.js
+++ cypress/integration/11_nahradzovanie_http_requestov/zaciatok.js
@@ -1,35 +1,36 @@
 /// <reference types="cypress" />
 
-beforeEach( () => {
+beforeEach(() => {
+  cy.server();
 
-  cy
-    .server()
+  cy.route({
+    method: "GET",
+    url: "/api/boards",
+    //fixtures emptyBoardList.json
+    // response: 'fx:emptyBoardList',
+    response: [],
+  }).as("boardList");
 
-  cy
-    .route('/api/boards')
-    .as('boardList')
+  cy.route({
+    method: "POST",
+    url: "/api/boards",
+    response: [],
+    status: 500,
+  }).as("createBoard");
 
-  cy
-    .visit('/');
-
+  cy.visit("/");
 });
 
-it.only('prazdny zoznam boardov', () => {
+it("prazdny zoznam boardov", () => {});
 
-})
+it("chybova hlaska pri vytvoreni boardu", () => {
+  cy.get("#new-board").click();
 
-it('chybova hlaska pri vytvoreni boardu', () => {
+  cy.get(".board_addBoard").type("nova zahrada");
 
-  cy
-    .get('#new-board')
-    .click()
+  cy.contains("Save").click();
 
-  cy
-    .get('.board_addBoard')
-    .type('nova zahrada')
-
-  cy
-    .contains('Save')
-    .click()
-
-})
+  cy.get("#errorMessage")
+    .should("be.visible")
+    .should("contain.text", "Oh no! New board could not be created.");
+});
--- cypress/integration/11_nahradzovanie_http_requestov/zoznamsa.js
+++ cypress/integration/11_nahradzovanie_http_requestov/zoznamsa.js
@@ -1,33 +1,39 @@
 /// <reference types="cypress" />
 
-beforeEach( () => {
+beforeEach(() => {
+  cy.server();
 
-  cy
-    .server()
+  //Solution for tasks 1, 2
+  // cy.route("/api/boards", "fx:twoBoards").as("boardList");
 
-  cy
-    .route('/api/boards')
-    .as('boardList')
+  cy.route({
+    method: "POST",
+    url: "/api/tasks",
+    response: [],
+    status: 500,
+  }).as("createBoard");
 
-  cy
-    .visit('/');
-
+  cy.visit("/");
 });
 
-it('zoznam dvoch boardov z fixture', () => {
-
+it("zoznam dvoch boardov z fixture", () => {
   // použi súbor twoBoards.json z fixtures foldra. do tohto it() bloku nemusíš nič písať. stačí, že zmeníš .route() príkaz v beforeEach bloku
+  //Boards sa načitaju z toho JSONU fixtures/twoBoards.json
+});
 
-})
-
-it('zoznam boardov s hviezdickou z fixture', () => {
-
+it("zoznam boardov s hviezdickou z fixture", () => {
   // ak sa Ti podarilo splniť predošlú úlohu, stačí si vyfiltrovať tento test a zmeniť dáta v súbore twoBoards.json po uložení testu a súboru twoBoards.json by si mal/a vidieť správny výsledok
+});
 
-})
-
-it('chyba pri vytvoreni tasku', () => {
-
+it("chyba pri vytvoreni tasku", () => {
   // pridaj si ďalší .route() príkaz, ktorým si odchytíš api volanie, ktoré sa robí pri vytvorení tasku. nahraď odpoveď tak, aby zo servra prišiel chybový status kód. dôležitá poznámka: pri nahradzovaní odpovede je parameter „response“ povinný
+  cy.get(".board_item").eq(0).click();
 
-})
+  cy.contains("Add a card").click();
+
+  cy.get(".List .TextArea").type("chlieb{enter}");
+
+  cy.get("#errorMessage")
+    .should("be.visible")
+    .should("contain.text", "There was an error creating task");
+});
