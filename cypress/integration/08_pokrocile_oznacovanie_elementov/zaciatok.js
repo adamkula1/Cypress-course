d454a35e90cea819be4f75c2087860447493df12
Adam Kula <adamkula97@gmail.com>
2022-11-20 14:02:18 +0100

    task #09 - first,last,child,parents

--- cypress/integration/08_pokrocile_oznacovanie_elementov/zaciatok.js
+++ cypress/integration/08_pokrocile_oznacovanie_elementov/zaciatok.js
@@ -1,36 +1,21 @@
 /// <reference types="cypress" />
 
-beforeEach( () => {
-
-  cy
-    .visit('/board/25619573353')
-
+beforeEach(() => {
+  cy.visit("/board/25619573353");
 });
 
-it('oznacenie tasku pomocou poradia', () => {
+it("oznacenie tasku pomocou poradia", () => {
+  cy.get(".Task").first();
+});
 
-  cy
-    .get('.Task')
+it("vyfiltrovanie názvov tasku", () => {
+  cy.get(".Task_title").filter(".completed");
+});
 
-})
+it("označenie tasku v zozname", () => {
+  cy.get(".List_tasks").children();
+});
 
-it('vyfiltrovanie názvov tasku', () => {
-
-  cy
-    .get('.Task_title')
-
-})
-
-it('označenie tasku v zozname', () => {
-
-  cy
-    .get('.List_tasks')
-
-})
-
-it('označenie zoznamu tasku', () => {
-
-  cy
-    .get('.Task')
-
-})
+it("označenie zoznamu tasku", () => {
+  cy.get(".Task").parents(".List");
+});
