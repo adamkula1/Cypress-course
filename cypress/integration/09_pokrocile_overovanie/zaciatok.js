74d0fe382db2fd0fc8cc1fb239ff4ac2ae80ef82
Adam Kula <adamkula97@gmail.com>
2022-11-20 14:37:00 +0100

    task #09 - then, expect, should

--- cypress/integration/09_pokrocile_overovanie/zaciatok.js
+++ cypress/integration/09_pokrocile_overovanie/zaciatok.js
@@ -1,35 +1,29 @@
 /// <reference types="cypress" />
 
-beforeEach( () => {
-
-  cy
-    .visit('/board/25619573353');
-
+beforeEach(() => {
+  cy.visit("/board/7115510273");
 });
 
-it('overenie textu prveho tasku', () => {
-
-  cy
-    .get('.Task')
-    .should('contain.text', 'chlieb')
-
-
+it("overenie textu prveho tasku", () => {
+  cy.get(".Task").should("contain.text", "vajce");
 });
 
-it('overenie textov vsetkych taskov', () => {
-
+it("overenie textov vsetkych taskov", () => {
+  cy.get(".Task").then((todos) => {
+    expect(todos[0]).to.contain.text("vajce");
+    expect(todos[1]).to.contain.text("chlieb");
+    expect(todos[2]).to.contain.text("voda");
+    expect(todos[3]).to.contain.text("pes");
+  });
 });
 
-it('task s textom „syr“ je na poslednej pozicii (riesenie 1)', () => {
-
-  cy
-    .get('.Task')
-    .last()
-    .should('contain.text', 'syr');
-
+it("task s textom „pes“ je na poslednej pozicii (riesenie 1)", () => {
+  cy.get(".Task").should("have.length", 4).last().should("contain.text", "pes");
 });
 
-it('task s textom „mlieko“ je na poslednej pozícii (riesenie 2)', () => {
-
+it("task s textom „pes“ je na poslednej pozícii (riesenie 2)", () => {
+  cy.get(".Task").should((todos) => {
+    expect(todos[0]).to.contain.text("vajce");
+    expect(todos[2]).to.contain.text("voda");
+  });
 });
-
--- cypress/integration/09_pokrocile_overovanie/zoznamsa.js
+++ cypress/integration/09_pokrocile_overovanie/zoznamsa.js
@@ -1,18 +1,32 @@
 /// <reference types="cypress" />
 
-beforeEach( () => {
-
-  cy
-    .visit(); // doplň si adresu svojho boardu
-
+beforeEach(() => {
+  cy.visit("/board/7115510273"); // doplň si adresu svojho boardu
 });
 
-it('over vsetky zaskrtnute tasky', () => {
+it("over vsetky zaskrtnute tasky", () => {
+  // cy.get(".Task").eq(0).find(".checkmark").check().should("be.checked");
+
+  // cy.get('.Task').then((todos) => {
+  //   expect(todos[0]).to.contain.text('vajce')
+  //   expect(todos[1]).to.contain.text('chlieb')
+  // })
+
+  // 2. použi príkaz .then() a over obidva tasky v zozname naraz
+  // cy.get('.Task input').then((checkboxes) => {
+  //   expect(checkboxes[0]).to.be.checked
+  //   expect(checkboxes[1]).to.not.be.checked
+  // })
+
+  // 4. použi príkaz .should() a task znova zaškrtni, aby Ti test prešiel počas toho ako test beží
+  cy.get('.Task input').should((checkboxes) => {
+    expect(checkboxes[0]).to.be.checked
+    expect(checkboxes[1]).to.not.be.checked
+  })
 
   // 1. zaškrtni si jeden z taskov
   // 2. použi príkaz .then() a over obidva tasky v zozname naraz
   // 3. skús jeden z taskov odškrtnúť tak, aby test neprechádzal
   // 4. použi príkaz .should() a task znova zaškrtni, aby Ti test prešiel počas toho ako test beží
   // poznámka: na vypracovanie tohto testu môžeš kontrolovať checkbox, alebo text tasku (ten má v zaškrtnutom stave classu .completed)
-
-})
+});
