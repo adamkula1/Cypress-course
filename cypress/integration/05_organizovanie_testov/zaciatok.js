08cee300460880e0111fd7c01522349a489086ad
Adam Kula <adamkula97@gmail.com>
2022-11-20 12:02:02 +0100

    task #05 - describe, beforeEach..

--- cypress/integration/05_organizovanie_testov/zaciatok.js
+++ cypress/integration/05_organizovanie_testov/zaciatok.js
@@ -1,9 +1,23 @@
 /// <reference types="cypress" />
 
-it('Test one', () => {
+describe("Organization test group 1", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
 
+  it("Test one", () => {});
+
+  it("Test two", () => {});
 });
+
+describe('group 2', () => {
+
+    it('Test three', () => {
     
-it('Test two', () => {
-      
-});
+    });
+        
+    it('Test four', () => {
+          
+    });
+  
+  })
