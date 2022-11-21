8dace657f3a67316ca3c1bf97ad1e4f0c84f8bb5
Adam Kula <adamkula97@gmail.com>
2022-11-21 10:22:47 +0100

    task #16 - user autorizacia - request GET and expect result, lodash

--- cypress/integration/16_autorizacia/zaciatok.js
+++ cypress/integration/16_autorizacia/zaciatok.js
@@ -1,8 +1,15 @@
 /// <reference types="cypress" />
 
-it('autorizacia', () => {
+it("autorizacia", () => {
+  cy.visit("/");
 
-  cy
-    .visit('/')
-  
+  cy.request({
+    method: "GET",
+    url: "/api/boards",
+    headers: {
+      accept: "application/json, text/plain, */*",
+      authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkYW1rdWxhOTdAZ21haWwuY29tIiwiaWF0IjoxNjY5MDIxOTQyLCJleHAiOjE2NjkwMjU1NDIsInN1YiI6IjEifQ.YXkgXavcM8fVXqBy0AM_q7L9Oc5iNVrnP6rwmskq2p8",
+    },
+  });
 });
--- cypress/integration/16_autorizacia/zoznamsa.js
+++ cypress/integration/16_autorizacia/zoznamsa.js
@@ -1,31 +1,35 @@
 /// <reference types="cypress" />
 
-it('overenie user id', () => {
+it("overenie user id", () => {
+  cy.visit("/");
 
-  cy
-    .visit('/')
+  cy.request({
+    method: "GET",
+    url: "/api/boards",
+    headers: {
+      accept: "application/json, text/plain, */*",
+      authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkYW1rdWxhOTdAZ21haWwuY29tIiwiaWF0IjoxNjY5MDIxOTQyLCJleHAiOjE2NjkwMjU1NDIsInN1YiI6IjEifQ.YXkgXavcM8fVXqBy0AM_q7L9Oc5iNVrnP6rwmskq2p8", // dopln autorizaciu
+    },
+  }).then((boards) => {
+    let privateBoard = Cypress._.find(boards.body, { name: "private board" });
+    let publicBoard = Cypress._.find(boards.body, { name: "verejny board" });
 
-  cy
-    .request({
-      method: 'GET', 
-      url: '/api/boards',
-      headers: {
-        accept: 'application/json, text/plain, */*',
-        authorization: '' // dopln autorizaciu
-      }
-    }).then( boards => {
+    expect(privateBoard.user).to.eq(1);
+    expect(publicBoard.user).to.eq(0);
+    expect(boards.body).to.have.length(3);
+  });
 
-    })
-  
-  cy
-    .request({
-      method: 'GET', 
-      url: '/api/boards',
-      headers: {
-        accept: 'application/json, text/plain, */*',
-      }
-    }).then( boards => {
-      
-    })
-  
+  cy.request({
+    method: "GET",
+    url: "/api/boards",
+    headers: {
+      accept: "application/json, text/plain, */*",
+    },
+  }).then((boards) => {
+    let publicBoard = Cypress._.find(boards.body, { name: "verejny board" });
+    
+    expect(publicBoard.user).to.eq(0);
+    expect(boards.body).to.have.length(2);
+  });
 });
