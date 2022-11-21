f58834cca31048b532f1f458d787675720beb4fd
Adam Kula <adamkula97@gmail.com>
2022-11-21 10:06:41 +0100

    task #15 - cookies settings

--- cypress/integration/15_cookies/zaciatok.js
+++ cypress/integration/15_cookies/zaciatok.js
@@ -1,8 +1,17 @@
 /// <reference types="cypress" />
 
-it('nastavovanie cookies', () => {
+//Cypress medzi testami cookies vymazáva
+//Pomocou tejto metody nastavíme whitelist všetkých cookies ktoré nechceme aby sa medzi testami vymazávali
+//Takéto nastavenia je vhodné vkladať do support/index.js
+// Cypress.Cookies.defaults({
+//   whitelist: "trello_token",
+// });
 
-  cy
-    .visit('/')
-    
-})
+it("nastavovanie cookies", () => {
+  cy.setCookie(
+    "trello_token",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkYW1rdWxhOTdAZ21haWwuY29tIiwiaWF0IjoxNjY5MDIxMjMyLCJleHAiOjE2NjkwMjQ4MzIsInN1YiI6IjEifQ.TYBgWKBWDj7dm5lvDe5_w4oNRRxQB3y3jW9Mh3-Q4AE"
+  );
+
+  cy.visit("/");
+});
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,9 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// Cypress.Cookies.defaults({
+//     whitelist: 'trello_token'
+//   })
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
